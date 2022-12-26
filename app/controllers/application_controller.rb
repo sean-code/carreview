f31351ac93acc5c3a45635232e06857633d896fe
sean-code <john.nganga@student.moringaschool.com>
2022-12-26 16:28:04 +0300

    Add CORS for access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
+
+    before do
+        response.headers['Access-Control-Allow-Origin']= "*"
+    end
 end
