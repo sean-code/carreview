39b6c9fb45a60b49e5d9044b3cc72590af5a1ace
sean-code <john.nganga@student.moringaschool.com>
2022-12-26 16:34:51 +0300

    Remove CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
 
-    before do
-        response.headers['Access-Control-Allow-Origin']= "*"
-    end
+    # before do
+    #     response.headers['Access-Control-Allow-Origin']= "*"
+    # end
 end
