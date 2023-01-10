9faceba52afd378a047dc66f4330a066b2c54618
sean-code <john.nganga@student.moringaschool.com>
2023-01-10 14:26:55 +0300

    Comment the has_many association at the car-serializer to not display the reviews

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,4 +1,5 @@
 class CarSerializer < ActiveModel::Serializer
   attributes :id, :make, :model, :price, :fuel_type, :transmission, :mileage, :engine_size, :year, :image
   # has_many :reviews
+  
 end
